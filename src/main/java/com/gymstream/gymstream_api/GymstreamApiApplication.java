a5102a6f25be03689e3076df8e13220b2319b21b
Stella <lopezlibrada437@gmail.com>
2026-04-12 20:19:57 -0300

    chore: configuracion inicial del proyecto con Spring Boot

---
+++ src/main/java/com/gymstream/gymstream_api/GymstreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.gymstream.gymstream_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymstreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymstreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymstream/gymstream_api/GymstreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymstream.gymstream_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymstreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
